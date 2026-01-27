6f96edd6ee57b0449ea514de4893b4dfa63e0cd5
Oka <hizrawandwioka@gmail.com>
2026-01-27 15:01:44 +0800

    refactor: add role and photo_url fields to User model in user.repository.js

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -5,6 +5,8 @@ const User = sequelize.define("User", {
   uid: { type: DataTypes.STRING, allowNull: false, unique: true },
   email: { type: DataTypes.STRING, allowNull: false },
   name: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "user" },
+  photo_url: { type: DataTypes.STRING },
 });
 
 export default User;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,3 +4,10 @@ import User from "../repositories/user.repository.js";
 export const getUserByUid = async (uid) => {
   return await User.findOne({ where: { uid } });
 };
+
+export const getAllUsers = async () => {
+  return await User.findAll({
+    attributes: ["id", "firebase_uid", "email", "name", "role", "photo_url", "createdAt", "updatedAt"],
+    order: [["createdAt", "DESC"]], 
+  });
+};
