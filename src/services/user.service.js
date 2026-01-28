558300a6d4878f89ed66703c092ce60261052f4a
Oka <hizrawandwioka@gmail.com>
2026-01-28 14:58:02 +0800

    fix: createuser wrong service name

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,3 +4,4 @@ export const createUser = (data) => userRepo.createUser(data);
 export const getUserById = (id) => userRepo.findById(id);
 export const getUserByEmail = (email) => userRepo.findByEmail(email);
 export const updateUser = (id, updates) => userRepo.updateUser(id, updates);
+export const createUserInDB = async (data) => User.create(data);
