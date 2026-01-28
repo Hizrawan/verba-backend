8809eca187f62791dadd14ab5c3482aa078a4379
Oka <hizrawandwioka@gmail.com>
2026-01-28 15:19:55 +0800

    fix: wrong course input

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getCourses, addCourse } from "../controllers/course.controller.js";
+import { getAllCourses, createCourse } from "../controllers/course.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getCourses);          
+router.get("/", createCourse);          
 //router.post("/", authenticate, createCourse); 
-router.post("/", addCourse); 
+router.post("/", getAllCourses); 
 
 export default router;
