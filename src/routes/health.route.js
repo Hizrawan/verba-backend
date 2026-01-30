ecb54d1f7dc283e972b681fe2bad663962f9de16
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 16:23:35 +0800

    Test github action

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-res.json({ status: "ok", message: "PRODUCTION API is healthy" });  
+res.json({ status: "ok", message: "PRODUCTION API IS WORKING PROPERLY" });  
 });
 
 export default router;
