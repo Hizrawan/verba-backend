bb5ea0fb366b8e6b3e0ace500da70a82b7897be2
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 15:29:30 +0800

    testing github action

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ port: process.env.PORT || 3000 || "production"});
+res.json({ status: "ok", message: "STAGING API is healthy" });  
 });
 
 export default router;
