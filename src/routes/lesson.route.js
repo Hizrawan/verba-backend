d1d9def4b62f5a376d18785250e5af4516c0f937
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 18:24:45 +0800

    Create CRUD function for lessons

--- src/routes/lesson.route.js
+++ src/routes/lesson.route.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { getLessonsByCourse, getLessonById, createLesson, updateLesson, deleteLesson} from "../controllers/lesson.controller.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ status: "ok" });
-});
+router.get("/", authenticate, getLessonsByCourse);
+router.get("/:id", authenticate, getLessonById);
+router.post("/", authenticate, createLesson);
+router.put("/:id", authenticate, updateLesson);
+router.delete("/:id", authenticate, deleteLesson);
 
 export default router;
