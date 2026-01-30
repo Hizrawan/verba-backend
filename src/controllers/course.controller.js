47f90c72bcab82378b1a81e2676caac190998952
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 18:04:41 +0800

    Create CRUD function for courses

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -8,3 +8,15 @@ export const getAllCourses = async (req, res, next) => {
   try { const courses = await courseService.getAllCourses(); res.json(courses); }
   catch (err) { next(err); }
 };
+export const getCourseById = async (req, res, next) => {
+  try { const course = await courseService.getCourseById(req.params.id); res.json(course); }
+  catch (err) { next(err); }
+};
+export const updateCourse = async (req, res, next) => {
+  try { const course = await courseService.updateCourse(req.params.id, req.body); res.json(course); }
+  catch (err) { next(err); }
+};
+export const deleteCourse = async (req, res, next) => {
+  try { await courseService.deleteCourse(req.params.id); res.status(204).end(); }
+  catch (err) { next(err); }
+};
--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getAllCourses, createCourse } from "../controllers/course.controller.js";
+import { getAllCourses, createCourse, getCourseById, updateCourse, deleteCourse } from "../controllers/course.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllCourses);          
-//router.post("/", authenticate, createCourse); 
-router.post("/", createCourse ); 
+router.get("/", getAllCourses);
+router.get("/:id", getCourseById);
+          
+router.post("/", authenticate, createCourse);
+router.put("/:id", authenticate, updateCourse);
+router.delete("/:id", authenticate, deleteCourse);
 
 export default router;
