bb503d78d2804be7bceb388480d2943c2c9cd055
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 17:25:16 +0800

    Test github action for staging server

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-res.json({ status: "ok", message: "PRODUCTION API is healthy" });  
+res.json({ status: "ok", message: "PRODUCTION API IS HEALTHY" });  
 });
 
 export default router;
