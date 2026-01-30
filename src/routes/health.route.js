fe2d69438a58e22a654c25e0a62e34f6592bfbf7
Hizrawan <Hizrawandwioka@gmail.com>
2026-01-30 15:12:22 +0800

    github action test

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ port: process.env.PORT || 3000 });
+  res.json({ port: process.env.PORT || 3000 || "production"});
 });
 
 export default router;
